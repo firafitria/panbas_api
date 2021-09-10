76b07570e715f4ebb0655762ceab0152ac78fd09
RiloArbabillah <r16615011@gmail.com>
2021-09-10 11:07:57 +0800

    add api laporan controller (index is done)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\LaporanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::group(['middleware => auth:sanctum'], function(){
 });
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/laporan/index', [LaporanController::class, 'index']);
